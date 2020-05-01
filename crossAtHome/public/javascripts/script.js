19d9f9cefc6fffcb90eee65ef006124742b0d5ab
Armando del Rio <d-rio@outlook.com>
2020-05-01 01:54:49 -0500

    wodPost model styled

--- crossAtHome/public/javascripts/script.js
+++ crossAtHome/public/javascripts/script.js
@@ -1,17 +1,15 @@
 document.addEventListener(
   'DOMContentLoaded',
   () => {
-    console.log(window.location.pathname !== '/');
     if (window.location.pathname !== '/') {
       const $logOutLi = document.createElement('li');
       $logOutLi.innerHTML = `<a class="nav-link text-info" href="/logout">LogOut</a>`;
       $logOutLi.classList.add('nav-item');
       $logOutLi.classList.add('nav-logOutLi');
       document.querySelector('.navbar-nav').appendChild($logOutLi);
-
     } else {
-      const $logOutLi = document.querySelector('.nav-logOutLi')
-      document.querySelector('.navbar-nav').removeChild($logOutLi);
+      const $logOutLi = document.querySelector('.nav-logOutLi');
+      if ($logOutLi) document.querySelector('.navbar-nav').removeChild($logOutLi);
     }
   },
   false
