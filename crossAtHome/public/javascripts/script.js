8742312b5e0bd9ebb210c412cbbcc7533bc0a5b2
Armando del Rio <d-rio@outlook.com>
2020-05-01 00:29:36 -0500

    added more styles, layout finished

--- crossAtHome/public/javascripts/script.js
+++ crossAtHome/public/javascripts/script.js
@@ -1,5 +1,18 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener(
+  'DOMContentLoaded',
+  () => {
+    console.log(window.location.pathname !== '/');
+    if (window.location.pathname !== '/') {
+      const $logOutLi = document.createElement('li');
+      $logOutLi.innerHTML = `<a class="nav-link text-info" href="/logout">LogOut</a>`;
+      $logOutLi.classList.add('nav-item');
+      $logOutLi.classList.add('nav-logOutLi');
+      document.querySelector('.navbar-nav').appendChild($logOutLi);
 
-  console.log('IronGenerator JS imported successfully!');
-
-}, false);
+    } else {
+      const $logOutLi = document.querySelector('.nav-logOutLi')
+      document.querySelector('.navbar-nav').removeChild($logOutLi);
+    }
+  },
+  false
+);
